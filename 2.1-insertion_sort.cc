12cacd51a52e3cde90f0f53138ed543b956718dc
Ruslan Akbashev <ruslashev@gmail.com>
2017-02-20 23:04:54 +0300

    add 2.1-3

--- 2.1-insertion_sort.cc
+++ 2.1-insertion_sort.cc
@@ -1,7 +1,7 @@
 #include "std.h"
 
 void insertion_sort(array &A) {
-  for (int j = 2; j <= A.length; j++) {
+  for (int j = 2; j <= A.length; ++j) {
     int key = A[j];
     int i = j - 1;
     while (i > 0 && key < A[i]) {
