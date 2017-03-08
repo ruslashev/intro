8c9787e23e16f7524234406b828d1f0d8c60c4cf
Ruslan Akbashev <ruslashev@gmail.com>
2017-03-08 16:07:38 +0300

    Better maximum subarray linear

--- 4.1-5-maximum_subarray_lin.cc
+++ 4.1-5-maximum_subarray_lin.cc
@@ -1,22 +1,32 @@
 #include "std.h"
 
 subarray find_maximum_subarray_lin(array &A) {
-  int best_sum = 0, best_now = 0, best_start_so_far = -1, best_stop_so_far = -1,
-      bestStartIndexNow = -1;
-  for (size_t i = 1; i <= A.length; ++i) {
-    int value = best_now + A[i];
-    if (value > 0) {
-      if (best_now == 0)
-        bestStartIndexNow = i;
-      best_now = value;
-    } else
-      best_now = 0;
-    if (best_now > best_sum) {
-      best_sum = best_now;
-      best_stop_so_far = i;
-      best_start_so_far = bestStartIndexNow;
+  // int max_ending_here = A[1], max_so_far = A[1];
+  // for (size_t i = 2; i <= A.length; ++i) {
+  //   int v = A[i];
+  //   max_ending_here = std::max(v, max_ending_here + v);
+  //   max_so_far = std::max(max_so_far, max_ending_here);
+  // }
+  // return max_so_far;
+  int max_sum_ending_here = A[1], max_sum_so_far = A[1];
+  int max_start_here = 1, max_end_here = 1,
+      max_start_so_far = 1, max_end_so_far = 1;
+  for (size_t i = 2; i <= A.length; ++i) {
+    int v = A[i];
+    if (v > max_sum_ending_here + v) {
+      max_sum_ending_here = v;
+      max_start_here = i;
+      max_end_here = i;
+    } else {
+      max_sum_ending_here = max_sum_ending_here + v;
+      max_end_here = i;
+    }
+    if (max_sum_ending_here > max_sum_so_far) {
+      max_sum_so_far = max_sum_ending_here;
+      max_start_so_far = max_start_here;
+      max_end_so_far = max_end_here;
     }
   }
-  return { best_start_so_far, best_stop_so_far, best_sum };
+  return { max_start_so_far, max_end_so_far, max_sum_so_far };
 }
 
